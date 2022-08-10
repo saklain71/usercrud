d90feadf824201c7666577ddfea437d05d19f514
Saklain Mustak <saklainmustak@sakmac.local>
2022-08-11 02:39:15 +0600

    navbar and footer done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 //import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login';
+import Footer from './Pages/Shared/Footer';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-  
-       <h1>For BD COM office</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="login" element={<Login></Login>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-center h-96 mt-20'>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
